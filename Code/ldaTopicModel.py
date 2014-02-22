922915a8c92f842b8f733da1cb1cdc3cf5d75575
Mohsen Malmir <mohsenmalmir@p1-m21.ucsd.edu>
2014-02-21 16:57:44 -0800

    first attempt for lda

--- Code/ldaTopicModel.py
+++ Code/ldaTopicModel.py
@@ -2,6 +2,9 @@ import numpy as np
 
 class ldaTopicModel:
     def __init__(self,**kwrds):
+        self.nTopics = 3
+        self.beta    = []
+        self.alpha   = []
         self.set_params(**kwrds)
 
     def fit_transform(self,X,y=None,**kwrds):
@@ -10,12 +13,71 @@ class ldaTopicModel:
 
     def fit(self,X,y=None,**kwrds):
         self.set_params(**kwrds)
+        self.trainCorpus(X)
 
     def set_params(self,**kwrds):
-        pass
-    
+        for k in kwrds:
+            if k=="n_topics":
+                self.nTopics = kwrds[k]
+            elif k=="topic_prior":
+                self.beta  = kwrds[k]
+            elif k=="document_prior":
+                self.alpha = kwrds[k]
+
+
+
     def get_params(self,deep=False):
         return dict()
     
     def transform(self,X):
         return None
+
+    #training code
+    def trainCorpus(self,X):
+        docs,words     = np.where(X!=0)
+        freq           = X[docs,words]
+        ndocs,dicSize  = X.shape
+        topicForWord   = np.zeros(freq.sum()) #[t] topic
+        #create count arrays
+        wordsInTopic        = np.zeros([self.nTopics,dicSize]) # [k,t] number of words t and topic k
+        topicsInDoc         = np.zeros([ndocs,self.nTopics])   # [m,k] number of topic k in document m
+        totalWordsInTopic   = np.zeros(self.nTopics)           # [k] number of words with topic k
+        totalTopicsInDoc    = np.zeros(ndocs)                  # [m] number of topics in document m
+        cnt = 0
+        #intialize counts
+        for d,w,f in zip(docs,words,freq):# document d with word w, with freq f
+            for ii in range(f):
+                z_m_n = np.where(np.random.multinomial(1, [1/3.]*3, size=1) != 0)[1][0]
+                wordsInTopic[z_m_n,w]    += 1 #
+                totalWordsInTopic[z_m_n] += 1
+                topicForWord[cnt]        = z_m_n
+                topicsInDoc[d,z_m_n]     += 1 #topic for document d
+                totalTopicsInDoc[d]     += 1
+                cnt += 1
+        converged = False
+        while not converged:
+        #do one round of gibbs sampling
+        #update counts
+            cnt = 0
+            for d,w,f in zip(docs,words,freq):# document d with word w, with freq f
+                for ii in range(f):
+                    topicsInDoc[d,topicForWord[cnt]]     -= 1
+                    wordsInTopic[topicForWord[cnt],w]    -= 1
+                    totalTopicsInDoc[d]                  -= 1
+                    totalWordsInTopic[topicForWord[cnt]] -= 1
+                    z_m_n = np.where(np.random.multinomial(1, , size=1) != 0)[1][0]
+                    prob = np.zeros(self.nTopics)
+                    for k in range(self.nTopics):
+                        prob[k] = (wordsInTopic[k,w]+self.beta[w]) * (topicsInDoc[d,k]+self.alpha[d])
+                        prob[k] /= (totalTopicsInDoc[d]+self.alpha)*(totalWordsInTopic[k])
+
+                    wordsInTopic[z_m_n,w]    += 1 #
+                    #                totalWordsInTopic[z_m_n] += 1
+                    topicForWord[cnt]        = z_m_n
+                    topicsInDoc[d,z_m_n]     += 1 #topic for document d
+                    #                totalTopicsInDoc[d]     += 1
+                    cnt += 1
+
+
+
+            #check for convergence
--- Code/runClassicData.py
+++ Code/runClassicData.py
@@ -2,7 +2,7 @@ import pprint
 import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
-
+from ldaTopicModel import ldaTopicModel
 
 #load classic data
 classic    = sio.loadmat("../Data/Classic/classic400.mat")
@@ -10,32 +10,6 @@ wordlist   = classic["classicwordlist"]
 wordFreq   = classic["classic400"]
 trueLabels = classic["truelabels"][0]
 wordFreq   = wordFreq.toarray()
-rows,cols  = np.where(wordFreq!=0)
-vals       = wordFreq[rows,cols]
-ndoc,nword = wordFreq.shape
-words      = np.arange(nword)
-count1,count2,count3 = [],[],[]
-for doc,word,freq in zip(rows,cols,vals):
-    if trueLabels[doc]==1:
-        for i in range(freq):
-            count1.append(word)
-    elif trueLabels[doc]==2:
-        for i in range(freq):
-            count2.append(word)
-    elif trueLabels[doc]==3:
-        for i in range(freq):
-            count3.append(word)
-#extract x,y
-#count1 = np.asarray(count1).reshape([1,-1])
-#count2 = np.asarray(count2).reshape([1,-1])
-#count3 = np.asarray(count3).reshape([1,-1])
-
-bins = np.arange(0,nword,30)
-
-plt.figure()
-#x = np.concatenate([count1,count2,count3],axis=0)
-#print x.shape
-plt.hist([count1,count2,count3], bins, histtype='bar', stacked=True, fill=True)
-#plt.hist(count2, bins, histtype='bar', stacked=True, fill=True)
-#plt.hist(count3, bins, histtype='bar', stacked=True, fill=True)
-plt.show()
+lda        = ldaTopicModel(n_topics = 3)
+X          = wordFreq
+lda.fit(X)
