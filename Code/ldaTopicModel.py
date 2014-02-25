d6b7748e3279b88d7c672b445a4809008d0d76bf
esayyari <erfan.sayyari@me.com>
2014-02-25 12:24:47 -0800

    ldaTopicModel, my opinion

--- Code/ldaTopicModel.py
+++ Code/ldaTopicModel.py
@@ -48,11 +48,17 @@ class ldaTopicModel:
         for d,w,f in zip(docs,words,freq):# document d with word w, with freq f
             for ii in range(f):
                 z_m_n = np.where(np.random.multinomial(1, [1/3.]*3, size=1) != 0)[1][0]
-                wordsInTopic[z_m_n,w]    += 1 #
+                #z[d][w][ii]=np.where(np.random.mutinomial((1, [1/3.]*3, size=1) != 0)[1][0]
+                wordsInTopic[z_m_n,w]    += 1
+               #wordsInTopic[z[d][w][ii],w]] +=1
                 totalWordsInTopic[z_m_n] += 1
+               #totalWordsInTopic[z[d][w][ii]] += 1
                 topicForWord[cnt]        = z_m_n
+               #topicForWord[d][w][ii] = z[d][w][ii]
                 topicsInDoc[d,z_m_n]     += 1 #topic for document d
+               #topicsInDoc[d,z[d][w][ii]]     += 1 #topic for document d
                 totalTopicsInDoc[d]     += 1
+               #totalTopicsInDoc[d]     += 1
                 cnt += 1
         converged = False
         while not converged:
@@ -62,20 +68,29 @@ class ldaTopicModel:
             for d,w,f in zip(docs,words,freq):# document d with word w, with freq f
                 for ii in range(f):
                     topicsInDoc[d,topicForWord[cnt]]     -= 1
+                   #topicsInDoc[d,topicForWord[d][w][ii]]     -= 1
                     wordsInTopic[topicForWord[cnt],w]    -= 1
+                   #wordsInTopic[topicForWord[d][w][ii],w]    -= 1
                     totalTopicsInDoc[d]                  -= 1
+                   #totalTopicsInDoc[d]                  -= 1
                     totalWordsInTopic[topicForWord[cnt]] -= 1
-                    z_m_n = np.where(np.random.multinomial(1, , size=1) != 0)[1][0]
+                   #totalWordsInTopic[topicForWord[d][w][ii]] -= 1
                     prob = np.zeros(self.nTopics)
                     for k in range(self.nTopics):
                         prob[k] = (wordsInTopic[k,w]+self.beta[w]) * (topicsInDoc[d,k]+self.alpha[d])
+                        #prob[k]=(wordsInTopic[topicForWord[d][w][ii],w]+self.beta[w])*(topicsInDoc[d,z[d][w][ii]]+self.alpha[d])
                         prob[k] /= (totalTopicsInDoc[d]+self.alpha)*(totalWordsInTopic[k])
-
+                    #prob[k]/= (topicsInDoc[d,z[d][w][ii]]+np.sum(self.alpha))*(totalWordsInTopic[topicForWord[d][w][ii]]+np.sum(self.beta))
+                    z_m_n = np.where(np.random.multinomial(1,prob, size=1) != 0)[1][0]
                     wordsInTopic[z_m_n,w]    += 1 #
                     #                totalWordsInTopic[z_m_n] += 1
+                    # totalWordsInTopic[z_m_n] +=1
                     topicForWord[cnt]        = z_m_n
+                    #topicForWord[d][w][ii] = z_m_n
                     topicsInDoc[d,z_m_n]     += 1 #topic for document d
+                    #topicsInDoc[d,z_m_n]     += 1 #topic for document d
                     #                totalTopicsInDoc[d]     += 1
+                    # totalTopicsInDoc[d] +=1
                     cnt += 1
 
 
