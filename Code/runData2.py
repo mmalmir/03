5fd697324f36c26e83655c7ab8f668b3f0175b54
Mohsen Malmir <mmalmir@ucsd.edu>
2014-02-27 00:35:11 -0800

    ...

--- Code/runData2.py
+++ Code/runData2.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
 from ldaTopicModel import ldaTopicModel
+from sklearn import manifold
 
 #load uci data
 data = sio.loadmat("../Data/dataset.mat")
@@ -22,32 +23,66 @@ for line in f:
     wordlist.append(line)
 f.close()
 wordlist = np.asarray(wordlist)
-#train classifier
-ntopics    =  2
-lda        = ldaTopicModel(n_topics = ntopics,alpha= (2./ntopics) * np.ones(ntopics),
-                                        beta=0.01*np.ones(nWords))
-#print X
 
-fname = "ldaUCI_%dtopics.20Iter.pickle"%ntopics
-############ TO SAVE, UNCOMMENT
-lda.fit(X)
-f = open(fname,"wb")
-pickle.dump(lda,f)
-f.close()
+ntopics    = 10
+nIter      = 1
+alpha      = 0.1
+beta       = .2
+train      = True
+lda        = ldaTopicModel(n_topics = ntopics,alpha= alpha * np.ones(ntopics),
+                                        beta=beta*np.ones(nWords),
+                                        nIter=nIter)
+
+fname = "uci_alpha%0.3f_beta%0.3f_iter%d.pickle"%(alpha,beta,nIter)
+
+if train:
+    lda.fit(X)
+    f = open(fname,"wb")
+    pickle.dump(lda,f)
+    f.close()
 
 
 ############ TO TEST, UNCOMMENT
-#f = open(fname,"rb")
-#lda = pickle.load(f)
-#f.close()
-#
+f = open(fname,"rb")
+lda = pickle.load(f)
+f.close()
+
 nwrd = 20
 idx = np.argsort(-lda.wordsInTopic)
 for i in range(ntopics):
     print "topic %d:"%i
-    print wordlist[idx[i,:nwrd]],
-##
-#print lda.topicsInDoc
-#
-#
-#
+    print wordlist[idx[0,:nwrd]],"\n"
+
+print "MDS..."
+
+theta = lda.topicsInDoc
+theta = theta / np.tile(theta.sum(axis=1).reshape([-1,1]),[1,ntopics])
+
+
+seed = np.random.RandomState(seed=3)
+mds = manifold.MDS(n_components=3, max_iter=3000, eps=1e-9, random_state=seed)
+pos = mds.fit(theta).embedding_
+
+nmds = manifold.MDS(n_components=3, metric=False, max_iter=3000, eps=1e-12,
+                    random_state=seed, n_jobs=1,n_init=1)
+theta = nmds.fit_transform(theta, init=pos)
+
+
+ax.scatter(theta[topic1Idx,0], theta[topic1Idx,1], theta[topic1Idx,2], c='r',label='True label=1')
+ax.scatter(theta[topic2Idx,0], theta[topic2Idx,1], theta[topic2Idx,2], c='g',label='True label=2')
+ax.scatter(theta[topic3Idx,0], theta[topic3Idx,1], theta[topic3Idx,2], c='b',label='True label=3')
+
+handles, labels = ax.get_legend_handles_labels()
+ax.legend()
+
+#ax.legend([h1,h2,h3], ["True label=1","True label=2","True label=3"])
+
+
+ax.set_xlabel("Topic 1")
+ax.set_ylabel("Topic 2")
+ax.set_zlabel("Topic 3")
+
+#plt.show()
+
+
+#calculate the percent correct
