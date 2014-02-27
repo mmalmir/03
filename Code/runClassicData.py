65e580b72cd0475b15c24d987d6402ed32a0a5dc
Mohsen Malmir <mmalmir@ucsd.edu>
2014-02-26 22:01:21 -0800

    ...

--- Code/runClassicData.py
+++ Code/runClassicData.py
@@ -1,5 +1,6 @@
 import pprint
 import pickle
+import copy
 import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
@@ -17,9 +18,9 @@ nWords     = 6205
 wordFreq   = wordFreq.toarray()
 
 
-nIter      = 200
-alpha      = 0.01
-beta       = 2.
+nIter      = 500
+alpha      = 0.1
+beta       = .2
 train      = True
 lda        = ldaTopicModel(n_topics = 3,alpha= alpha * np.ones(3),
                                         beta=beta*np.ones(nWords),
@@ -28,7 +29,7 @@ X          = wordFreq
 
 fname = "lda400_alpha%0.3f_beta%0.3f_iter%d.pickle"%(alpha,beta,nIter)
 
-fname = "lda400_alpha.1_beta2_200itr.pickle"
+#fname = "lda400_alpha.1_beta2_200itr.pickle"
 
 if train:
     lda.fit(X)
@@ -57,16 +58,18 @@ ax = fig.add_subplot(111, projection='3d')
 #    xs = randrange(n, 23, 32)
 #    ys = randrange(n, 0, 100)
 #    zs = randrange(n, zl, zh)
-print trueLabels
+#print trueLabels
 topic1Idx = np.where(trueLabels==1)
 topic2Idx = np.where(trueLabels==2)
 topic3Idx = np.where(trueLabels==3)
 
-h1 = ax.scatter(theta[topic1Idx,0], theta[topic1Idx,1], theta[topic1Idx,2], c='r',label="True label=1")
-h2 = ax.scatter(theta[topic2Idx,0], theta[topic2Idx,1], theta[topic2Idx,2], c='g',label="True label=2")
-h3 = ax.scatter(theta[topic3Idx,0], theta[topic3Idx,1], theta[topic3Idx,2], c='b',label="True label=3")
+ax.scatter(theta[topic1Idx,0], theta[topic1Idx,1], theta[topic1Idx,2], c='r',label='True label=1')
+ax.scatter(theta[topic2Idx,0], theta[topic2Idx,1], theta[topic2Idx,2], c='g',label='True label=2')
+ax.scatter(theta[topic3Idx,0], theta[topic3Idx,1], theta[topic3Idx,2], c='b',label='True label=3')
 
-#handles, labels = ax.get_legend_handles_labels()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend()
+
 #ax.legend([h1,h2,h3], ["True label=1","True label=2","True label=3"])
 
 
@@ -74,8 +77,23 @@ ax.set_xlabel("Topic 1")
 ax.set_ylabel("Topic 2")
 ax.set_zlabel("Topic 3")
 
-plt.show()
+#plt.show()
 
+
+#calculate the percent correct
+
+topics = np.argmax(theta,axis=1)
+
+rates = []
+for i in range(1,4):
+    for j in range(1,4):
+        for k in range(1,4):
+            t1 = copy.deepcopy(topics)
+            t1[np.where(topics==0)] = i
+            t1[np.where(topics==1)] = j
+            t1[np.where(topics==2)] = k
+            rates.append( (t1==trueLabels).mean() )
+print np.max(rates)
 
 
 
